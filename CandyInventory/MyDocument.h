b559926e404a078226c68ac4373411594f42d618
Geraldo Nascimento <gnascim@gmail.com>
2010-06-19 19:03:11 +0100

    CandyInventory from hotcocoa.lastedit.com. Wherein CoreData is introduced.

---
+++ CandyInventory/MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  CandyInventory
+//
+//  Created by Geraldo Nascimento on 6/19/10.
+//  Copyright Addition 2010 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
---
+++ CandyInventory/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CandyInventory
+//
+//  Created by Geraldo Nascimento on 6/19/10.
+//  Copyright Addition 2010 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
